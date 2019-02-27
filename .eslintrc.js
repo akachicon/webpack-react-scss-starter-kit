07c15ae1d67b2e42c81509ce7cf863dca0a77941
Dan Z <akachicon@gmail.com>
2019-02-27 13:30:07 +0300

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  extends: 'airbnb',
+  env: {
+    browser: true
+  },
+  rules: {
+    'no-underscore-dangle': 0,
+    'comma-dangle': 0
+  },
+  settings: {
+    'import/resolver': {
+      webpack: {
+        config: './webpack-config/webpack.config.base.js'
+      }
+    }
+  }
+};
---
+++ src/components/app.jsx
@@ -0,0 +1,10 @@
+import { hot } from 'react-hot-loader/root';
+import React, { Component } from 'react';
+
+const App = () => (
+  <h1>
+    Hello react!
+  </h1>
+);
+
+export default hot(App);
